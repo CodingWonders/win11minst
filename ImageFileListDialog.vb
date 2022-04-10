55acd1ca138869ec35f51e1ddc919bb079aa254c
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2022-04-10 18:34:28 +0200

    Added (these are used to list files from installers)

---
+++ ImageFileListDialog.vb
@@ -0,0 +1,8 @@
+﻿Imports System.Windows.Forms
+
+Public Class ImageFileListDialog
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+End Class
