4d6fba9757af34cb295796c8ad43579bdc51c5f8
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2022-04-10 18:36:24 +0200

    Added: this changes the user-agent of ISOFileDownloadPanel.WebComponent

---
+++ UserAgentChanger.vb
@@ -0,0 +1,17 @@
+﻿Module UserAgentChanger
+    <Runtime.InteropServices.DllImport("urlmon.dll", CharSet:=Runtime.InteropServices.CharSet.Ansi)>
+    Private Function UrlMkSetSessionOption(
+        ByVal dwOption As Integer,
+        ByVal pBuffer As String,
+        ByVal dwBufferLength As Integer,
+        ByVal dwReserved As Integer) As Integer
+    End Function
+
+    Const URLMON_OPTION_USERAGENT As Integer = &H10000001
+    Const URLMON_OPTION_USERAGENT_REFRESH As Integer = &H10000002
+
+    Public Sub SetUserAgent(ByVal UserAgent As String)
+        UrlMkSetSessionOption(URLMON_OPTION_USERAGENT_REFRESH, vbNullString, 0, 0)
+        UrlMkSetSessionOption(URLMON_OPTION_USERAGENT, UserAgent, UserAgent.Length, 0)
+    End Sub
+End Module
