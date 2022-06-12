695c748f4d88c01943a5f7f6002e5414b5b31390
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2022-06-12 17:44:27 +0200

    Assembly information

--- My Project/Resources.Designer.vb
+++ My Project/Resources.Designer.vb
@@ -323,6 +323,26 @@ Namespace My.Resources
         '''<summary>
         '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property export_dark() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("export_dark", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property export_light() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("export_light", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property file_create_fluent() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("file_create_fluent", resourceCulture)
