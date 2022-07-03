6f4ff4d93e81867080661f92e2fc8951fc59666c
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2022-07-03 17:07:34 +0200

    PUCS access files for the Windows 11 Manual Installer

--- upd/PassiveUpdateCheckSys/Module1.vb
+++ upd/PassiveUpdateCheckSys/Module1.vb
@@ -5,7 +5,7 @@ Module Module1
 
     Sub Main()
         Console.Title = "Passive Update Check System"
-        Console.WriteLine("Passive Update Check System (PUCS) version 0.5, for the Windows 11 Manual Installer")
+        Console.WriteLine("Passive Update Check System (PUCS) version {0}", My.Application.Info.Version.ToString() & ", for the Windows 11 Manual Installer")
         Console.WriteLine(CrLf & _
                           "  Beginning program update. Please wait...")
         Try
@@ -29,12 +29,15 @@ Module Module1
                 File.Delete(".\new\win11minst.exe")
             End If
             File.Move(".\win11minst.exe", ".\win11minst_old_v" & OldVerStr & ".exe")
+            BackUpSettings()
             Process.Start("powershell", "Copy-Item -Path .\new\*.* -Destination . -Recurse -Force").WaitForExit()
             Process.Start("powershell", "Remove-Item -Path .\Resources -Recurse -Force").WaitForExit()
             Directory.CreateDirectory(".\Resources")
             Process.Start("powershell", "Copy-Item -Path .\new\Resources\* -Destination .\Resources -Recurse -Force").WaitForExit()
             File.Move(".\win11minst_new.exe", ".\win11minst.exe")
             Process.Start("powershell", "Remove-Item -Path .\new -Recurse -Force").WaitForExit()
+            DeleteTmpSettings()
+            RestoreSettings()
             Thread.Sleep(3000)
             Process.Start(".\win11minst.exe")
             Console.WriteLine(CrLf & _
@@ -50,4 +53,43 @@ Module Module1
         End Try
     End Sub
 
+    Sub BackUpSettings()
+        File.Move(".\settings.ini", ".\settings.ini.back")
+        If File.Exists(".\InstName.ini") And _
+            File.Exists(".\TargetInstaller.ini") And _
+            File.Exists(".\Win11Inst.ini") And _
+            File.Exists(".\Win10Inst.ini") Then
+            ' Check this if settings.ini contains "ReuseSI=1". If it doesn't, skip this
+            File.Move(".\InstName.ini", ".\InstName.ini.back")
+            File.Move(".\TargetInstaller.ini", ".\TargetInstaller.ini.back")
+            File.Move(".\Win11Inst.ini", ".\Win11Inst.ini.back")
+            File.Move(".\Win10Inst.ini", ".\Win10Inst.ini.back")
+        End If
+    End Sub
+
+    Sub DeleteTmpSettings()
+        File.Delete(".\settings.ini")
+        If File.Exists(".\InstName.ini") And _
+            File.Exists(".\TargetInstaller.ini") And _
+            File.Exists(".\Win11Inst.ini") And _
+            File.Exists(".\Win10Inst.ini") Then
+            File.Delete(".\InstName.ini")
+            File.Delete(".\TargetInstaller.ini")
+            File.Delete(".\Win11Inst.ini")
+            File.Delete(".\Win10Inst.ini")
+        End If
+    End Sub
+
+    Sub RestoreSettings()
+        File.Move(".\settings.ini.back", ".\settings.ini")
+        If File.Exists(".\InstName.ini.back") And _
+            File.Exists(".\TargetInstaller.ini.back") And _
+            File.Exists(".\Win11Inst.ini.back") And _
+            File.Exists(".\Win10Inst.ini.back") Then
+            File.Move(".\InstName.ini.back", ".\InstName.ini")
+            File.Move(".\TargetInstaller.ini.back", ".\TargetInstaller.ini")
+            File.Move(".\Win11Inst.ini.back", ".\Win11Inst.ini")
+            File.Move(".\Win10Inst.ini.back", ".\Win10Inst.ini")
+        End If
+    End Sub
 End Module
