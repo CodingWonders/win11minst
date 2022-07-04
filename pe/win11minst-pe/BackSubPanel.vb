2df64a892921359871e284cbc3f35b1b7089b7ac
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2022-07-04 15:30:13 +0200

    This is not useful

--- pe/win11minst-pe/BackSubPanel.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class BackSubPanel
-
-    Private Sub BackSubPanel_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Me.Size = MainForm.Size
-        Me.Location = MainForm.Location
-    End Sub
-End Class
