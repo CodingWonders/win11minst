d283adafa7f8aca4cd6b39a69652de37c7b7ee44
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2022-07-24 20:11:42 +0200

    Remove SV2 options

--- Panels/AdvancedOptionsPanel.Designer.vb
+++ Panels/AdvancedOptionsPanel.Designer.vb
@@ -29,10 +29,6 @@ Partial Class AdvancedOptionsPanel
         Me.Label1 = New System.Windows.Forms.Label()
         Me.CheckBox1 = New System.Windows.Forms.CheckBox()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
         Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -97,7 +93,7 @@ Partial Class AdvancedOptionsPanel
         '
         'Label2
         '
-        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI Semibold", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label2.Location = New System.Drawing.Point(54, 123)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(629, 76)
@@ -105,62 +101,13 @@ Partial Class AdvancedOptionsPanel
         Me.Label2.Text = "Bypasses Microsoft Account sign-in and forced Internet connection setup on Window" & _
     "s 11 Pro (Nickel builds 22557 onwards)"
         '
-        'CheckBox2
-        '
-        Me.CheckBox2.AutoSize = True
-        Me.CheckBox2.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.CheckBox2.Location = New System.Drawing.Point(37, 202)
-        Me.CheckBox2.Name = "CheckBox2"
-        Me.CheckBox2.Size = New System.Drawing.Size(408, 24)
-        Me.CheckBox2.TabIndex = 5
-        Me.CheckBox2.Text = "Hide ""System requirements not met"" watermark (22557+)"
-        Me.CheckBox2.UseVisualStyleBackColor = True
-        '
-        'Label4
-        '
-        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(53, 229)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(629, 44)
-        Me.Label4.TabIndex = 6
-        Me.Label4.Text = "Hides the ""System requirements not met"" watermark on Nickel builds 22557 onwards " & _
-    "and Windows Server ""Copper"" builds 25057 onwards"
-        '
-        'Label5
-        '
-        Me.Label5.AutoSize = True
-        Me.Label5.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.ForeColor = System.Drawing.Color.Crimson
-        Me.Label5.Location = New System.Drawing.Point(53, 273)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(513, 20)
-        Me.Label5.TabIndex = 6
-        Me.Label5.Text = "Enabling this option is not recommended yet, as it doesn't work as intended."
-        '
-        'LinkLabel1
-        '
-        Me.LinkLabel1.AutoSize = True
-        Me.LinkLabel1.Font = New System.Drawing.Font("Segoe UI", 11.25!)
-        Me.LinkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline
-        Me.LinkLabel1.LinkColor = System.Drawing.Color.FromArgb(CType(CType(1, Byte), Integer), CType(CType(92, Byte), Integer), CType(CType(186, Byte), Integer))
-        Me.LinkLabel1.Location = New System.Drawing.Point(53, 293)
-        Me.LinkLabel1.Name = "LinkLabel1"
-        Me.LinkLabel1.Size = New System.Drawing.Size(312, 20)
-        Me.LinkLabel1.TabIndex = 7
-        Me.LinkLabel1.TabStop = True
-        Me.LinkLabel1.Text = "Read the full issue and a possible workaround"
-        '
         'AdvancedOptionsPanel
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(720, 480)
-        Me.Controls.Add(Me.LinkLabel1)
-        Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.CheckBox2)
         Me.Controls.Add(Me.CheckBox1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Panel1)
@@ -184,9 +131,5 @@ Partial Class AdvancedOptionsPanel
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
     Friend WithEvents Label2 As System.Windows.Forms.Label
-    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
-    Friend WithEvents Label4 As System.Windows.Forms.Label
-    Friend WithEvents Label5 As System.Windows.Forms.Label
-    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
 
 End Class
--- Panels/AdvancedOptionsPanel.vb
+++ Panels/AdvancedOptionsPanel.vb
@@ -9,11 +9,6 @@ Public Class AdvancedOptionsPanel
         Else
             MainForm.UseBypassNRO = False
         End If
-        If CheckBox2.Checked = True Then
-            MainForm.UseSV2 = True
-        Else
-            MainForm.UseSV2 = False
-        End If
         Me.DialogResult = System.Windows.Forms.DialogResult.OK
         Me.Close()
         BackSubPanel.Close()
@@ -27,66 +22,41 @@ Public Class AdvancedOptionsPanel
 
     Private Sub AdvancedOptionsPanel_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         CheckBox1.Enabled = My.User.IsInRole(ApplicationServices.BuiltInRole.Administrator)
-        CheckBox2.Enabled = My.User.IsInRole(ApplicationServices.BuiltInRole.Administrator)
         If MainForm.ComboBox4.SelectedItem = "English" Or MainForm.ComboBox4.SelectedItem = "Inglés" Or MainForm.ComboBox4.SelectedItem = "Anglais" Then
             CheckBox1.Text = "Bypass Microsoft Account sign-in and forced Internet connection setup (22557+)"
-            CheckBox2.Text = "Hide " & Quote & "System requirements not met" & Quote & " watermark (22557+)"
             Label1.Text = "Advanced options"
             Label2.Text = "Bypasses Microsoft Account sign-in and forced Internet connection setup on Windows 11 Pro (Nickel builds 22557 onwards)"
-            Label4.Text = "Hides the " & Quote & "System requirements not met" & Quote & " watermark on Nickel builds 22557 onwards and Copper builds 25115 onwards"
-            Label5.Text = "Enabling this option is not recommended yet, as it doesn't work as intended."
-            LinkLabel1.Text = "Read the full issue and a possible workaround"
             OK_Button.Text = "OK"
             Cancel_Button.Text = "Cancel"
         ElseIf MainForm.ComboBox4.SelectedItem = "Spanish" Or MainForm.ComboBox4.SelectedItem = "Español" Or MainForm.ComboBox4.SelectedItem = "Espagnol" Then
             CheckBox1.Text = "Omitir inicio de sesión con la cuenta de Microsoft y configuración forzada de Internet (22557+)"
-            CheckBox2.Text = "Ocultar la marca de agua " & Quote & "Requisitos de sistema no cumplidos" & Quote & " (22557+)"
             Label1.Text = "Opciones avanzadas"
             Label2.Text = "Omite el inicio de sesión con la cuenta de Microsoft y la configuración forzada de Internet en Windows 11 Pro (compilaciones de Nickel 22557 en adelante)"
-            Label4.Text = "Oculta la marca de agua " & Quote & "Requisitos de sistema no cumplidos" & Quote & " en compilaciones de Nickel 22557 en adelante y Copper 25115 en adelante"
-            Label5.Text = "Todavía no es recomendable habilitar esta opción, ya que no funciona como se esperaba."
-            LinkLabel1.Text = "Lea la publicación completa y una posible solución"
             OK_Button.Text = "Aceptar"
             Cancel_Button.Text = "Cancelar"
         ElseIf MainForm.ComboBox4.SelectedItem = "French" Or MainForm.ComboBox4.SelectedItem = "Francés" Or MainForm.ComboBox4.SelectedItem = "Français" Then
             CheckBox1.Text = "Contourner l'ouverture de session du compte Microsoft et la configuration forcée de la connexion Internet (22557+)"
-            CheckBox2.Text = "Masquer le filigrane " & Quote & "Configuration requise non respectée" & Quote & " (22557+)"
             Label1.Text = "Options avancées"
             Label2.Text = "Contournement de l'ouverture de session du compte Microsoft et de la configuration forcée de la connexion Internet sur Windows 11 Pro (Nickel builds 22557 et suivants)"
-            Label4.Text = "Masque le filigrane " & Quote & "Configuration requise non respectée" & Quote & " sur les builds 22557 et suivantes de Nickel et 25115 et suivantes de Copper"
-            Label5.Text = "L'activation de cette option n'est pas encore recommandée, car elle ne fonctionne pas comme prévu."
-            LinkLabel1.Text = "Lisez l'intégralité du problème et une solution de contournement possible."
             OK_Button.Text = "OK"
             Cancel_Button.Text = "Annuler"
         ElseIf MainForm.ComboBox4.SelectedItem = "Automatic" Or MainForm.ComboBox4.SelectedItem = "Automático" Or MainForm.ComboBox4.SelectedItem = "Automatique" Then
             If My.Computer.Info.InstalledUICulture.ThreeLetterWindowsLanguageName = "ENG" Then
                 CheckBox1.Text = "Bypass Microsoft Account sign-in and forced Internet connection setup (22557+)"
-                CheckBox2.Text = "Hide " & Quote & "System requirements not met" & Quote & " watermark (22557+)"
                 Label1.Text = "Advanced options"
                 Label2.Text = "Bypasses Microsoft Account sign-in and forced Internet connection setup on Windows 11 Pro (Nickel builds 22557 onwards)"
-                Label4.Text = "Hides the " & Quote & "System requirements not met" & Quote & " watermark on Nickel builds 22557 onwards and Copper builds 25115 onwards"
-                Label5.Text = "Enabling this option is not recommended yet, as it doesn't work as intended."
-                LinkLabel1.Text = "Read the full issue and a possible workaround"
                 OK_Button.Text = "OK"
                 Cancel_Button.Text = "Cancel"
             ElseIf My.Computer.Info.InstalledUICulture.ThreeLetterWindowsLanguageName = "ESN" Then
                 CheckBox1.Text = "Omitir inicio de sesión con la cuenta de Microsoft y configuración forzada de Internet (22557+)"
-                CheckBox2.Text = "Ocultar la marca de agua " & Quote & "Requisitos de sistema no cumplidos" & Quote & " (22557+)"
                 Label1.Text = "Opciones avanzadas"
                 Label2.Text = "Omite el inicio de sesión con la cuenta de Microsoft y la configuración forzada de Internet en Windows 11 Pro (compilaciones de Nickel 22557 en adelante)"
-                Label4.Text = "Oculta la marca de agua " & Quote & "Requisitos de sistema no cumplidos" & Quote & " en compilaciones de Nickel 22557 en adelante y Copper 25115 en adelante"
-                Label5.Text = "Todavía no es recomendable habilitar esta opción, ya que no funciona como se esperaba."
-                LinkLabel1.Text = "Lea la publicación completa y una posible solución"
                 OK_Button.Text = "Aceptar"
                 Cancel_Button.Text = "Cancelar"
             ElseIf My.Computer.Info.InstalledUICulture.ThreeLetterWindowsLanguageName = "FRA" Then
                 CheckBox1.Text = "Contourner l'ouverture de session du compte Microsoft et la configuration forcée de la connexion Internet (22557+)"
-                CheckBox2.Text = "Masquer le filigrane " & Quote & "Configuration requise non respectée" & Quote & " (22557+)"
                 Label1.Text = "Options avancées"
                 Label2.Text = "Contournement de l'ouverture de session du compte Microsoft et de la configuration forcée de la connexion Internet sur Windows 11 Pro (Nickel builds 22557 et suivants)"
-                Label4.Text = "Masque le filigrane " & Quote & "Configuration requise non respectée" & Quote & " sur les builds 22557 et suivantes de Nickel et 25115 et suivantes de Copper"
-                Label5.Text = "L'activation de cette option n'est pas encore recommandée, car elle ne fonctionne pas comme prévu."
-                LinkLabel1.Text = "Lisez l'intégralité du problème et une solution de contournement possible."
                 OK_Button.Text = "OK"
                 Cancel_Button.Text = "Annuler"
             End If
@@ -96,29 +66,14 @@ Public Class AdvancedOptionsPanel
             BackColor = Color.White
             ForeColor = Color.Black
             Panel1.BackColor = Color.FromArgb(243, 243, 243)
-            LinkLabel1.LinkColor = Color.FromArgb(1, 92, 186)
             OK_Button.BackColor = Color.FromArgb(1, 92, 186)
             OK_Button.ForeColor = Color.White
         ElseIf MainForm.BackColor = Color.FromArgb(32, 32, 32) Then
             BackColor = Color.FromArgb(43, 43, 43)
             ForeColor = Color.White
             Panel1.BackColor = Color.FromArgb(32, 32, 32)
-            LinkLabel1.LinkColor = Color.FromArgb(76, 194, 255)
             OK_Button.BackColor = Color.FromArgb(76, 194, 255)
             OK_Button.ForeColor = Color.Black
         End If
-    End Sub
-
-    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
-        If CheckBox2.Checked = True Then
-            MsgBox("This option is experimental, and it doesn't work as intended. Please expect this to work in a future update.", vbOKOnly + vbInformation, "Experimental option")
-            If DialogResult.OK Then
-                CheckBox2.Checked = True
-            End If
-        End If
-    End Sub
-
-    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
-        Process.Start("https://github.com/CodingWonders/win11minst/issues/2")
     End Sub
 End Class
